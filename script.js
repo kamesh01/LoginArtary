8c5af6169cb5b8a90e730375d62f07526c3c6805
kamesh01 <kameshkhandekar@gmail.com>
2019-08-08 14:51:11 +0530

    Login page for time pass

---
+++ script.js
@@ -0,0 +1,17 @@
+const email = document.querySelector('[data-email]');
+const password = document.querySelector('[data-pass]');
+//  Eye balls
+// const password = document.querySelector('[data-pass]');
+// const password = document.querySelector('[data-pass]');
+// Wings
+const wings = document.querySelector('[data-wings]');
+
+// const password = document.querySelector('[data-pass]');
+wings.pseudoStyle("before","display","none");
+
+
+
+
+email.addEventListener('keydown', () =>{
+    
+});
